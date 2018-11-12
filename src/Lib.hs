fca91ae3ea325f522f14a4fd64c3a0924e72642d
Oliver Braun <ob@cs.hm.edu>
2018-11-12 10:14:10 +0100

    Stand Vorlesung

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,7 +17,15 @@ bigNumber :: (Ord x, Num x) => x -> x -> String
 bigNumber x y = if x > 1000 || y /= x then "biiiiiggg" else "small"
 
 
-data Day = Monday | Tuesday | Sunday
+data Day = Mon | Tue | Wed | Thu | Fri | Sat | Sunday
+  deriving (Eq, Show, Read, Ord, Enum, Bounded)
+
+myFavDay :: Day -> Bool
+myFavDay Sat = True
+
+type DayOfWeek = Int
+data Date = Date DayOfWeek Day
+
 
 f' :: (Double, Double) -> Bool
 f' _ = True
@@ -33,10 +41,17 @@ tupFun t@(a, _) (c, _) = if a < c then (a + c, "a war kleiner") else t
 fst4 :: (Int, b, c, d) -> Int
 fst4 (x, _, _, _) = x + 2
 
+add'' :: Num a => (a, a) -> a
+add'' (x,y) = x + y
 
 
+data Trivial = Trivial
+  deriving (Eq, Show)
 
+myAbs :: Int -> Int
+myAbs x = if x < 0 then -x else x
 
-
-
-
+myAbs' 2 = 2
+myAbs' x
+  | x < 0 = -x
+myAbs' x = x
